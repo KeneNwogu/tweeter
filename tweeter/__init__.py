19c1bae1a6cb41ecdfd22e5376797640917759c7
[Kene Nwogu] <[nwogujoseph1111@gmail.com]>
2022-01-28 19:57:45 +0100

    cloudinary global config correction

--- tweeter/__init__.py
+++ tweeter/__init__.py
@@ -11,11 +11,12 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'you-dey-waste-your-t
 app.config['UPLOAD_FOLDER'] = 'tweeter/media'
 app.config['MONGO_URI'] = os.environ.get('MONGO_URI') or 'mongodb://127.0.0.1:27017/tweeter_test'
 app.config['CLOUDINARY_URL'] = os.environ.get('CLOUDINARY_URL')
-cloudinary.config.update = ({
-    'cloud_name': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'api_key': os.environ.get('CLOUDINARY_API_KEY'),
-    'api_secret': os.environ.get('CLOUDINARY_API_SECRET')
-})
+
+cloudinary.config(
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET')
+)
 
 
 ALLOWED_EXTENSIONS = {'.png', '.jpg', '.jpeg', '.mp4', '.mp3'}
